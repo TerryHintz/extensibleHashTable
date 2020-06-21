0557fa2b6512003641e02405bb0f268e97402bb7
Leo Zhang <cza82@sfu.ca>
2020-06-21 16:11:18 -0700

    insert done

--- ExtensibleHashTable.cpp
+++ ExtensibleHashTable.cpp
@@ -1,6 +1,6 @@
 #include "ExtensibleHashTable.h"
 #include <iostream>
-#include <math.h>
+#include <cmath>
 
 using namespace std;
 
@@ -16,25 +16,25 @@ bool ExtensibleHashTable::find(int val){
 };
 
 void ExtensibleHashTable::insert(int val){
-    int hashed = hashFunc(val);
-    int index = getLastBits(hashed, globalDepth);
+    int index = hashFunc(val);
+    // int index = getLastBits(hashed, globalDepth);
     Bucket * bucket = hashTable[index];
     // cout << val << " " << index << endl;
     if(!bucket->isFull()){
         bucket->insert(val);
     } else if (globalDepth == bucket->localDepth){
-        cout << endl << endl;
-        print();
-        cout << endl << endl;
+        // cout << endl << endl;
+        // print();
+        // cout << endl << endl;
         doubleSize();
-        cout << endl << endl;
-        cout << "index" << index << endl;
-        print();
-        cout << endl << endl;
+        // cout << endl << endl;
+        // cout << "index" << index << endl;
+        // print();
+        // cout << endl << endl;
         
         split(bucket, index);
-        cout << endl << endl;
-        print();
+        // cout << endl << endl;
+        // print();
         insert(val);
     } else {
         split(bucket, index);
@@ -59,7 +59,8 @@ ExtensibleHashTable::~ExtensibleHashTable(){
 }
 
 int ExtensibleHashTable::hashFunc(int val){
-    return val*val;
+    int size = pow(2, globalDepth);
+    return val%size;
 }
 
 int ExtensibleHashTable::getLastBits(int val, int depth){
@@ -73,11 +74,11 @@ void ExtensibleHashTable::doubleSize(){
 
     Bucket** newHashTable = new Bucket*[newBuckets];
 
-    for (int i=0; i<oldBuckets; i++) {
-        newHashTable[i] = hashTable[i];
-    }
-    for (int i=oldBuckets; i<newBuckets; i++) {
-        newHashTable[i] = new Bucket(size);
+    // for (int i=0; i<oldBuckets; i++) {
+    //     newHashTable[i] = hashTable[i];
+    // }
+    for (int i=0; i<newBuckets; i++) {
+        newHashTable[i] = hashTable[i%oldBuckets];
     }
 
     delete[] hashTable;
@@ -86,15 +87,17 @@ void ExtensibleHashTable::doubleSize(){
 }
 
 void ExtensibleHashTable::split(Bucket* bucket, int index){
+    int newLocalDepth = ++bucket->localDepth;
     hashTable[index] = new Bucket(bucket->size);
-    hashTable[index]->localDepth = ++bucket->localDepth;
+    hashTable[index]->localDepth = newLocalDepth;
     int oldSize = pow(2, globalDepth-1);
-    hashTable[(index+oldSize)]->localDepth++;
+    hashTable[(index+oldSize)] = new Bucket(bucket->size);
+    hashTable[(index+oldSize)]->localDepth = newLocalDepth;
 
     for(int i=0; i<bucket->count; i++){
         int val = bucket->arr[i];
-        int hashed = hashFunc(val);
-        int index = getLastBits(hashed, bucket->localDepth);
+        int index = hashFunc(val);
+        // int index = getLastBits(hashed, bucket->localDepth);
         hashTable[index]->insert(val);
     }
 
--- main.cpp
+++ main.cpp
@@ -23,13 +23,38 @@ void test2(){
 }
 
 void test3(){
-    ExtensibleHashTable temp = ExtensibleHashTable();
-    temp.insert(4);
+    ExtensibleHashTable temp = ExtensibleHashTable(3);
+    temp.insert(64);
     temp.print();
+    cout << endl;
+    temp.insert(66);
+    temp.print();
+    cout << endl;
+    temp.insert(200);
+    temp.print();
+    cout << endl;
+    temp.insert(13);
+    temp.print();
+    cout << endl;
+    temp.insert(67);
+    temp.print();
+    cout << endl;
+    temp.insert(218);
+    temp.print();
+    cout << endl;
+    temp.insert(253);
+    temp.print();
+    cout << endl;
+    temp.insert(153);
+    temp.print();
+    cout << endl;
+    temp.insert(109);
+    temp.print();
+    cout << endl;
 }
 
 int main(){
     // test1();
-    test2();
+    test3();
     return 0;
 }
